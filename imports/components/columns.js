e13626a6b29bfeb5903b780aa04e7cc1a2cfabd4
MataiulS <camargodecastromatheus@gmail.com>
2020-07-12 16:56:00 -0300

    columns in a separated file

---
+++ imports/components/columns.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Link } from '@reach/router';
+import { Space, Tag } from 'antd';
+import moment from 'moment';
+import Dinero from 'dinero.js';
+import { deleteAccountingEntry } from '../api/methods/deleteAccountingEntry';
+import { getInstallmentNumber } from '../api/installments';
+import { Categories } from '../api/categories';
+import { Accounts } from '../api/accounts';
+
+export const columns = (entries) => [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    sorter: ({ name: a }, { name: b }) => a.localeCompare(b),
+    render: (text) => text,
+  },
+  {
+    title: 'Categories',
+    key: 'categoryIds',
+    dataIndex: 'categoryIds',
+    render: (categorieIdArray) => {
+      let names = [];
+      categorieIdArray.forEach((catId) => {
+        const categorie = Categories.findOne(catId);
+        names.push(categorie.name);
+      });
+      return (
+        <>
+          {names.map((name) => {
+            const color = name.length > 5 ? 'geekblue' : 'green';
+            return (
+              <Tag color={color} key={name}>
+                {name.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      );
+    },
+  },
+  {
+    // TODO ----> re-render after use set payed button.
+    title: 'Account',
+    dataIndex: 'accountId',
+    key: 'accountId',
+    render: (accountId) => {
+      const account = Accounts.findOne(accountId);
+      return account.name;
+    },
+    defaultSortOrder: 'descend',
+    sorter: ({ accountId: a }, { accountId: b }) => {
+      const [accountA, accountB] = Accounts.find({
+        _id: { $in: [a, b] },
+      }).fetch();
+      return accountA?.name.localeCompare(accountB?.name);
+    },
+    filters: entries.map(({ accountId }) => {
+      const account = Accounts.find((acc) => acc._id === accountId);
+      return {
+        text: account?.name,
+        value: accountId,
+      };
+    }),
+    onFilter: (value, record) => record.accountId === value,
+  },
+  {
+    title: '# of Installments',
+    key: 'installment',
+    render: ({ dueDate, purchaseDate }, record) =>
+      getInstallmentNumber(purchaseDate, dueDate),
+  },
+  {
+    title: 'Due date',
+    dataIndex: 'dueDate',
+    key: 'dueDate',
+    defaultSortOrder: 'descend',
+    sorter: ({ dueDate: a }, { dueDate: b }) => a.valueOf() - b.valueOf(),
+    render: (value, entry) => moment(value).format('DD/MM/YYYY'),
+  },
+  {
+    title: 'Value',
+    dataIndex: 'value',
+    key: 'value',
+    defaultSortOrder: 'descend',
+    sorter: ({ value: a }, { value: b }) => a - b,
+    render: (value) => Dinero({ amount: value }).toFormat(),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (record) => (
+      <Space size="middle">
+        <a
+          onClick={() => {
+            const { _id } = record;
+            // eslint-disable-next-line no-restricted-globals
+            const confirmation = confirm('Are you sure?');
+            if (confirmation) {
+              deleteAccountingEntry.run(_id);
+            }
+          }}
+        >
+          Delete
+        </a>
+        <Link
+          to={`/new-accounting-entry/credit/${record._id}`}
+          className="nav-text"
+        >
+          Edit
+        </Link>
+      </Space>
+    ),
+  },
+];
--- imports/ui/Dashboard.jsx
+++ imports/ui/Dashboard.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 // eslint-disable-next-line import/no-unresolved
 import { useTracker } from 'meteor/react-meteor-data';
-import { Space, Spin, Tag } from 'antd';
-
-import Dinero from 'dinero.js';
-import { Link } from '@reach/router';
+import { Spin } from 'antd';
 import moment from 'moment';
-import Tabs from 'antd/es/tabs';
 import { Categories } from '../api/categories';
 import { markAsPayed } from '../api/methods/markAsPayed';
 import { AccountingEntries } from '../api/accountingEntries';
 import { Accounts } from '../api/accounts';
 import { useDashboardData } from './helpers/dashboardHelpers';
 import { DashboardTable } from '../components/DashboardTable';
-import {
-  getInstallmentNumber,
-  InstallmentsCollection,
-} from '../api/installments';
-import { deleteAccountingEntry } from '../api/methods/deleteAccountingEntry';
+import { InstallmentsCollection } from '../api/installments';
+import { columns } from '../components/columns';
 
 export const Dashboard = ({
   year = moment().format('YYYY'),
@@ -72,113 +65,6 @@ export const Dashboard = ({
     };
     markAsPayed.call(obj);
   };
-
-  const columns = (entries) => [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      sorter: ({ name: a }, { name: b }) => a.localeCompare(b),
-      render: (text) => text,
-    },
-    {
-      title: 'Categories',
-      key: 'categoryIds',
-      dataIndex: 'categoryIds',
-      render: (categorieIdArray) => {
-        let names = [];
-        categorieIdArray.forEach((catId) => {
-          const categorie = categoriesArray.find((cat) => cat._id === catId);
-          names.push(categorie.name);
-        });
-        return (
-          <>
-            {names.map((name) => {
-              const color = name.length > 5 ? 'geekblue' : 'green';
-              return (
-                <Tag color={color} key={name}>
-                  {name.toUpperCase()}
-                </Tag>
-              );
-            })}
-          </>
-        );
-      },
-    },
-    {
-      // TODO ----> re-render after use set payed button.
-      title: 'Account',
-      dataIndex: 'accountId',
-      key: 'accountId',
-      render: (accountId) => {
-        const account = accountsArray.find((acc) => acc._id === accountId);
-        return account.name;
-      },
-      defaultSortOrder: 'descend',
-      sorter: ({ accountId: a }, { accountId: b }) => {
-        const [accountA, accountB] = accountsArray.filter(
-          (acc) => acc._id === a || acc._id === b
-        );
-        return accountA?.name.localeCompare(accountB?.name);
-      },
-      filters: entries.map(({ accountId }) => {
-        const account = accountsArray.find((acc) => acc._id === accountId);
-        return {
-          text: account?.name,
-          value: accountId,
-        };
-      }),
-      onFilter: (value, record) => record.accountId === value,
-    },
-    {
-      title: '# of Installments',
-      key: 'installment',
-      render: ({ dueDate, purchaseDate }, record) =>
-        getInstallmentNumber(purchaseDate, dueDate),
-    },
-    {
-      title: 'Due date',
-      dataIndex: 'dueDate',
-      key: 'dueDate',
-      defaultSortOrder: 'descend',
-      sorter: ({ dueDate: a }, { dueDate: b }) => a.valueOf() - b.valueOf(),
-      render: (value, entry) => moment(value).format('DD/MM/YYYY'),
-    },
-    {
-      title: 'Value',
-      dataIndex: 'value',
-      key: 'value',
-      defaultSortOrder: 'descend',
-      sorter: ({ value: a }, { value: b }) => a - b,
-      render: (value) => Dinero({ amount: value }).toFormat(),
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (record) => (
-        <Space size="middle">
-          <a
-            onClick={() => {
-              const { _id } = record;
-              // eslint-disable-next-line no-restricted-globals
-              const confirmation = confirm('Are you sure?');
-              if (confirmation) {
-                deleteAccountingEntry.run(_id);
-              }
-            }}
-          >
-            Delete
-          </a>
-          <Link
-            to={`/new-accounting-entry/credit/${record._id}`}
-            className="nav-text"
-          >
-            Edit
-          </Link>
-        </Space>
-      ),
-    },
-  ];
 
   return (
     <>
